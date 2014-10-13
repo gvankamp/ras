408495023ef499d935d305ed4b86408b61c8e1e8
gvankamp <gvankamp@outlook.com>
2014-10-13 12:16:12 +0200

    add ras function

---
+++ ras.R
@@ -0,0 +1,24 @@
+ras <- function(m, colgoal, rowgoal, iterations = 5000){
+ 
+  
+  repeat{
+  
+    m <-  diag(getrowcor(m, rowgoal)) %*% m
+    m <-  m %*% diag(getcolcor(m, colgoal))
+    
+    iterations <- iterations - 1
+    if(iterations == 0) break
+  }
+  m
+}
+
+getcolcor <- function(m, colgoal){
+   
+  colgoal/colSums(m)
+}
+
+getrowcor <- function(m, rowgoal){
+  
+  rowgoal/rowSums(m)
+ 
+}
