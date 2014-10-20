4894b9b7368fa847e67c6b17717d22d1ea7797b9
gvankamp <gvankamp@outlook.com>
2014-10-20 16:05:19 +0200

    add input check
    check if input is dataframe, convert to matrix if  so

--- ras.R
+++ ras.R
@@ -1,9 +1,15 @@
 ras <- function(m, colgoal, rowgoal, iterations = 100){
- 
+
+  ## check input, if dataframe -> convert to matrix
+  if(class(m)!="matrix") 
+  
+    {m <- data.matrix(m)}
+
+  
   repeat{
   
-    m <-  diag(getrowcor(m, rowgoal)) %*% m
-    m <-  m %*% diag(getcolcor(m, colgoal))
+    m <-  (diag(getrowcor(m, rowgoal))%*%m) 
+    m <-  (m %*% diag(getcolcor(m, colgoal)))
     
     iterations <- iterations - 1
     if(iterations == 0) break
